6fc065c1c1db89af85080f722c2d619fc2dfa614
cutlerwater <cutlerwater2@live.com>
2021-11-04 15:29:11 -0400

    login and registering works

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,31 @@
 
 <script>
 import Navigation from "./components/Navigation.vue";
+import { ref } from "vue";
+import { supabase } from './supabase/init';
+import store from "./store/index";
+
 export default {
   components: {
     Navigation,
     },
   setup() {
     // Create data / vars
+    const appReady = ref(null);
     // Check to see if user is already logged in
+     const user = supabase.auth.user();
     // If user does not exist, need to make app ready
+    if (!user) {
+      appReady.value = true;
+    }
     // Runs when there is a auth state change
     // if user is logged in, this will fire
-    return {};
+    supabase.auth.onAuthStateChange((_, session) => {
+      console.log("hi garrett!");
+      store.methods.setUser(session);
+      appReady.value = true;
+    });
+    return { appReady };
   },
 };
 </script>
